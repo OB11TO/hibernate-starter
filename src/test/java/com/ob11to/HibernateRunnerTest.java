a1340d79810bcfa0f787430833523f9baf81c2b8
OB11TO <OB11TO61@yandex.ru>
2022-06-09 12:57:13 +0300

    Hibernate. 6. Класс Session Подключил junit Демонстрация того, что происходит под капотом +-

---
+++ src/test/java/com/ob11to/HibernateRunnerTest.java
@@ -0,0 +1,63 @@
+package com.ob11to;
+
+import com.ob11to.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+class HibernateRunnerTest {
+
+    @Test
+    void checkReflectionApi() throws SQLException, IllegalAccessException {
+        User user = User.builder()
+                .username("ivan@gmail.com")
+                .firstname("Ivan")
+                .lastname("Ivanov")
+                .birthDate(LocalDate.of(2000, 12, 11))
+                .age(20)
+                .build();
+
+        String sql = "insert " +
+                "into " +
+                "%s " +
+                "(%s) " +
+                "values " +
+                "(%s)";
+
+        var tableName = Optional.ofNullable(user.getClass().getAnnotation(Table.class))
+                .map(tableAnnotation -> tableAnnotation.schema() + "." + tableAnnotation.name())
+                .orElse(user.getClass().getName());
+
+        Field[] fields = user.getClass().getDeclaredFields();
+
+        var columnNames = Arrays.stream(fields)
+                .map(field -> Optional.ofNullable(field.getAnnotation(Column.class))
+                        .map(Column::name)
+                        .orElse(field.getName()))
+                .collect(Collectors.joining(", "));
+
+        String columnValues = Arrays.stream(fields)
+                .map(field -> "?")
+                .collect(Collectors.joining(", "));
+
+        System.out.printf((sql) + "%n", tableName, columnNames, columnValues);
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = connection.prepareStatement(String.format(sql, tableName, columnNames, columnValues));
+        for (Field field : fields) {
+            field.setAccessible(true); //разрешить менять поля
+            preparedStatement.setObject(1, field.get(user));
+        }
+
+    }
+
+}
